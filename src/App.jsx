d1bc1bc57fd2174ab33edd77c798b447eac225db
Nauman <naumanumer10@outlook.com>
2023-10-28 18:20:21 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import conf from './conf/conf'
+function App() {
+  
+console.log("ddd",conf.appWriteUrl)
+  return (
+    <>
+    <h1>Hello</h1></>
+  )
+}
+
+export default App

6fbe6cde1ce603bf3ccd77aa61e32ed6ad2dbd71
Nauman <naumanumer10@outlook.com>
2023-11-10 23:40:29 +0000

    Adding Components To Main File

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import conf from './conf/conf'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+//import {login,logout} from './store/authSlice'
+
+import { login ,logout} from './store/authSlice';
+import  Footer  from './components/Footer/Footer';
+import  Header  from './components/Header/Header';
 function App() {
-  
-console.log("ddd",conf.appWriteUrl)
+console.log(conf.appWriteUrl)
+//console.log("environment variable",import.meta.env.VITE_APPWRITE_URL);
+
+const [loading,setLoading]=useState(true);
+const dispatch=useDispatch()
+
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then ((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout());
+
+    }
+   
+  })
+  .finally(()=>setLoading(false))
+},[])
+return  !loading?(
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className="w-full block
+    ">
+
+
+      <Header>
+        <main>
+        {/* <Outlet></Outlet> */}
+        </main>
+        </Header>
+      <Footer></Footer>
+    </div>
+  </div>
+):null
+
+
   return (
     <>
     <h1>Hello</h1></>

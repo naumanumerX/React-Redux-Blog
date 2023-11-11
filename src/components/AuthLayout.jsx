5812debd1ca326a5bf20f3fa42eb4c6256bfb654
Nauman <naumanumer10@outlook.com>
2023-11-11 19:20:36 +0000

    Authentication For Login / Logout

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,33 @@
+import React, { useState ,useEffect} from 'react'
+
+import { UseSelector, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function Protected ({children,authentication=true}) {
+
+const navigate=useNavigate()
+const [loader,setLoader]=useState(true)
+const authStatus=useSelector(state=>state.auth.status);
+
+    
+useEffect(()=>{
+        if(authentication && authStatus!==authentication){
+            navigate("/login")
+        }
+        else if(!authentication && authStatus!==authentication)
+        {
+            navigate("/")
+        }
+
+} , [authStatus,navigate,authentication])
+  return (
+    <>
+    
+    {loader ? <h1>Loading...</h1> : children} 
+    
+    
+    </>
+  )
+}
+

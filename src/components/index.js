88fe8ae585ae4872fc6d3f2e77f72cc6f610ccc5
Nauman <naumanumer10@outlook.com>
2023-11-17 21:22:54 +0000

    Adding Form And  Slug Values

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Header from "./Header/Header";
 
 import Footer from "./Footer/Footer";
 import Container from "./container/container";
+import Logout from "./Logout";
 import Logo from "./Logo";
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    Logout,
+    RTE
 }
